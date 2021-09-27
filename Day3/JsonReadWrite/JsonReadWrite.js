e80e7c44e3196d87a409566dcf30e78832fda4bf
abhishek-shigavan <abhishekshigavan1437@gmail.com>
2021-09-27 19:16:20 +0530

    Refactor: JSON file conversion to .csv and .xlsm is implemented

--- Day3/JsonReadWrite/JsonReadWrite.js
+++ Day3/JsonReadWrite/JsonReadWrite.js
@@ -1,41 +1,42 @@
-const fs = require('fs');
+const fileSystem = require('fs');
+const JsonToCsv = require('json2csv').parse;
+const JsonToXlsx = require('xlsx');
 
-//new object for adding in JSON file 
+function getJsonObj(inputFile) {
+    let stringJson = fileSystem.readFileSync(inputFile);
+    let jsonObj = JSON.parse(stringJson);
+    return jsonObj;
+}
+
 const newStudent = {
     fname: "Peter",
     lname: "Parkar",
     state: "U.P"
 };
 
-fs.readFile('./student.json', 'utf-8', (err, jsonString) => {
-    if (err) {
-        console.log(err);
-    }
-    else {
-        try {
-            //converting json string to js object
-            let student = JSON.parse(jsonString);
-            console.log(student);
-            //appending new data
-            student.push(newStudent);
-            const jsonStudent = JSON.stringify(student, null, 2);
+function updateAndCreateJson(oldFilePath, newData, newFilePath) {
+    let jsonObj = getJsonObj(oldFilePath);
+    jsonObj.push(newData);
+    let jsonInString = JSON.stringify(jsonObj, null, 2);
+    fileSystem.writeFileSync(newFilePath, jsonInString)
+}
 
-            //writing into another file
-            fs.writeFile('./updatedStudent.json', jsonStudent, (err) => {
-                if (err) {
-                    console.log(err);
-                }
-                else {
-                    console.log("New data added");
-                }
-            })
-        } catch (err) {
-            console.log('error occured', err);
-        }
-    }
-});
+function jsonToCsv(jsonFile, csvFile) {
+    let jsonObj = getJsonObj(jsonFile);
+    const csvData = JsonToCsv(jsonObj, { fields: ["fname", "lname", "state"] });
+    fileSystem.writeFileSync(csvFile, csvData)
+}
 
+function jsonToXlsx(jsonFile, xlsxFile) {
+    let jsonObj = getJsonObj(jsonFile);
+    //creating a workbook
+    let workBook = JsonToXlsx.utils.book_new();
+    //creting worksheet
+    let workSheet = JsonToXlsx.utils.json_to_sheet(jsonObj);
+    JsonToXlsx.utils.book_append_sheet(workBook, workSheet, 'Student Data');
+    JsonToXlsx.writeFile(workBook, xlsxFile);
+}
 
-
-
-
+updateAndCreateJson('./student.json', newStudent, './updatedStudent.json');
+jsonToCsv('./student.json', './student.csv');
+jsonToXlsx('./student.json', './student.xlsx');
